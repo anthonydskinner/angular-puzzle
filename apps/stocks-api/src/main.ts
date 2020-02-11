4e45fb1ed6e532cde479e223906fffb1271e40d1
anthony skinner <anthony.skinner@sapientrazorfish.com>
2020-02-11 14:10:41 -0600

    created/moved stock cache to a plugin Added some styles

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,59 +3,16 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
-import { environment } from './environments/environment';
-import CatboxMemory from '@hapi/catbox-memory';
-import fetch from 'node-fetch';
+import { StocksCachePlugin } from './app/hapi-plugin/stocks-cache';
 
 const init = async () => {
   const server = new Server({
     port: 3333,
-    host: 'localhost',
-    cache: [
-      {
-        name: 'stock-cache',
-        provider: {
-          constructor: CatboxMemory,
-          options: {
-            partition: 'stock-cache-data'
-          }
-        }
-      }
-    ]
+    host: 'localhost'
   });
-
-  const fetchStocks = async symbol => {
-    const url = `${environment.apiURL}/beta/stock/${symbol.id}/chart/max?token=${environment.apiKey}`;
-
-    return await fetch(url)
-      .then( response => response.json())
-      .then (data => {
-        return data;
-      })
-      .catch(error => {
-        return error
-      });
-
-  };
-
-  const stocksCache = server.cache({
-    cache: 'stock-cache',
-    expiresIn: 10 * 1000,
-    segment: 'stocksSegment',
-    generateFunc: async id => {
-      return await fetchStocks(id);
-    },
-    generateTimeout: 2000
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/api/beta/stock/{symbol}/chart/max',
-    handler: async (request, h) => {
-      const symbol = request.params.symbol;
-      const id = `${symbol}`;
-      return await stocksCache.get({ id });
-    }
+  
+  await server.register({
+    plugin: StocksCachePlugin
   });
 
   await server.start();
